79340955b541213959af523943a4e3a48bab8ce0
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-02-04 21:21:55 +0100

    Code update

---
+++ src/com/vitexsoftware/netbeans/modules/php/versionswitch/Installer.java
@@ -0,0 +1,35 @@
+/**
+ * PHPSwitch module Installer
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ * @copyright 2021 Vitex@hippy.cz (G)
+ */
+package com.vitexsoftware.netbeans.modules.php.versionswitch;
+
+import org.openide.modules.ModuleInstall;
+import org.openide.util.Exceptions;
+
+/**
+ *
+ * @author vitex
+ */
+public class Installer extends ModuleInstall {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void restored() {
+    }
+
+    @Override
+    public void close() {
+        try {
+            // if other exceptions are thrown when IDE is closed, the options may not be saved
+            // so sleep a bit
+            Thread.sleep(100);
+        } catch (InterruptedException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+
+}
--- src/com/vitexsoftware/netbeans/modules/php/versionswitch/ToolbarSwitch.java
+++ src/com/vitexsoftware/netbeans/modules/php/versionswitch/ToolbarSwitch.java
@@ -1,7 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * PHPSwitch ToolbarButton class
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ * @copyright 2021 Vitex@hippy.cz (G)
  */
 package com.vitexsoftware.netbeans.modules.php.versionswitch;
 
